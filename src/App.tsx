c6b76ab6bc75230b796b749b8ed522de2eea6e9e
SMelidoni <simone.melidoni@gmail.com>
2025-06-28 19:01:20 +0100

    feat: initialise AOS library in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import './App.css'
 import LandingPage from './components/LandingPage'
+import 'aos/dist/aos.css'
+import AOS from 'aos'
+import { useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 600,
+      once: true,
+      easing: 'ease-in-out',
+    })
+  }, [])
+
   return (
     <>
       <div>
